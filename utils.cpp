e3ce1b3c09154e7d5271f9f74016a686f3d8e28e
mkluk <mgrinz.official@o2.pl>
2018-12-05 23:35:38 +0100

    fadeIn działa na Shape i Sprite (#koszmar)

--- utils.cpp
+++ utils.cpp
@@ -1,31 +1,21 @@
 #include "utils.h"
 
+using namespace std;
+
 void fadeIn(Shape &object, int duration, int maxTransparency)
 {
-	static map<Shape*, Clock*> clockMap;
-
 	Color color = object.getFillColor();
-	
-	if (color.a >= maxTransparency)
-		return;
 
-	Clock *clock;
-
-	if (!clockMap.count(&object))
-	{
-		clock = new Clock();
-		clockMap.insert(pair<Shape*, Clock*>(&object, clock));
-	}
-	else
-		clock = clockMap.at(&object);
-
-	color.a = ( maxTransparency / duration) * clock->getElapsedTime().asMilliseconds();
-	
-	if (clock->getElapsedTime().asMilliseconds() > duration)
-	{
-		color.a = maxTransparency;
-		clockMap.erase(&object);
-	}
+	fadeIn1<Shape>(object, duration, color, maxTransparency);
 	
 	object.setFillColor(color);
+}
+
+void fadeIn(Sprite &object, int duration, int maxTransparency)
+{
+	Color color = object.getColor();
+
+	fadeIn1<Sprite>(object, duration, color, maxTransparency);
+	
+	object.setColor(color);
 }
--- utils.h
+++ utils.h
@@ -5,4 +5,32 @@
 using namespace sf;
 using namespace std;
 
+template <class T>
+void fadeIn1(T &object, int duration, Color &color, double maxTransparency = 255)
+{
+	static map<T*, Clock*> clockMap;
+
+	if (color.a >= maxTransparency)
+		return;
+
+	Clock *clock;
+
+	if (!clockMap.count(&object))
+	{
+		clock = new Clock();
+		clockMap.insert(pair<T*, Clock*>(&object, clock));
+	}
+	else
+		clock = clockMap.at(&object);
+
+	color.a = (maxTransparency / duration) * clock->getElapsedTime().asMilliseconds();
+
+	if (clock->getElapsedTime().asMilliseconds() > duration)
+	{
+		color.a = maxTransparency;
+		clockMap.erase(&object);
+	}
+}
+
 void fadeIn(Shape &object, int duration, int maxTransparency = 255);
+void fadeIn(Sprite &object, int duration, int maxTransparency = 255);
